7008a5e54207ccad654fbd595b7b8a94702a544f
Felippe Alves de Paiva <felippeapaiva@gmail.com>
2022-07-14 10:39:04 -0700

    added some comments for clarification

--- src/BigBox.js
+++ src/BigBox.js
@@ -1,5 +1,4 @@
-import React, { useEffect } from 'react'
-import { useState } from 'react';
+import React from 'react'
 
 const BigBox = ({ colorValue }) => {
 
@@ -10,6 +9,7 @@ const BigBox = ({ colorValue }) => {
   return (
     <section
         className='square'
+        // for the color to change based on the input.
         style={{ backgroundColor: colorValue }}
     >
         <p>{colorValue ? colorValue : "Empty Value"}</p>
